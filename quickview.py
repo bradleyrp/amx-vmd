dc2228a50faaa3b4930c25b54811c52f9bda0e48
Ryan Bradley <bradleyrp@gmail.com>
2017-03-07 10:18:50 -0500

    added quickview

--- quickview.py
+++ quickview.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import re,os,tempfile
+import os,sys,re,tempfile,json
 
 __all__ = ['martiniview']
 
@@ -12,14 +12,21 @@ def martiniview(*args,**kwargs):
 	#---! hardcoded-path
 	cg_bonds_fn = '~/libs/cg_bonds.tcl'
 	gmxdump_fn = 'inputs/martini/bin/gmxdump'
-	def parse(ext):
-		matches = [i for i in args if os.path.splitext(i)[1]==('.'+ext)]
-		if len(matches)>1: raise Exception('too many matches to extension %s in the command line'%ext)
-		if not matches: return None
-		return matches[0]
-	gro = kwargs.get('gro',parse('gro'))
-	tpr = kwargs.get('tpr',parse('tpr'))
-	xtc = kwargs.get('xtc',parse('xtc'))
+	#---! first we check for state.json
+	if os.path.isfile('state.json'):
+		sys.path.insert(0,'')
+		import amx
+		last = amx.get_last_gmx_call('mdrun')
+		gro,tpr,xtc = [os.path.join(amx.state.here,last['flags'][i]) for i in ['-c','-s','-x']]
+	else:
+		def parse(ext):
+			matches = [i for i in args if os.path.splitext(i)[1]==('.'+ext)]
+			if len(matches)>1: raise Exception('too many matches to extension %s in the command line'%ext)
+			if not matches: return None
+			return matches[0]
+		gro = kwargs.get('gro',parse('gro'))
+		tpr = kwargs.get('tpr',parse('tpr'))
+		xtc = kwargs.get('xtc',parse('xtc'))
 	if not all([xtc,gro,tpr]): raise Exception('missing one of gro (%s) tpr (%s)'%(gro,tpr))
 	cmds = ['mol new %s'%gro]
 	if xtc: cmds += ['mol addfile %s'%xtc]
